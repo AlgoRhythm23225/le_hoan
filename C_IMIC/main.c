8407f79bff0f8696b5fca4baab6af11d2d3e0c04
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-06 20:41:13 +0700

    Number to char

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,24 +3,40 @@
 
 void main()
 {
-	float a;
-	printf("Nhap diem trung binh: ");
-	scanf("%f", &a);
-	if (a > 10)
-	{
-		printf("Diem khong hop le");
-	}
-	else if (a >= 8)
-	{
-		printf("Loai gioi");
-	}
-	else if (a >= 6.5)
-	{
-		printf("Loai kha");
-	}
-	else
-	{
-		printf("Loai trung binh");
-	}
-	
+	int n;
+		printf("Enter a number: ");
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:
+			printf("one");
+			break;
+		case 2:
+			printf("two");
+			break;
+		case 3:
+			printf("three");
+			break;
+		case 4:
+			printf("four");
+			break;
+		case 5:
+			printf("five");
+			break;
+		case 6:
+			printf("six");
+			break;
+		case 7:
+			printf("seven");
+			break;
+		case 8:
+			printf("eight");
+			break;
+		case 9:
+			printf("nine");
+			break;
+		default:
+			printf("Greater than 9");
+			break;
+		}
 }
