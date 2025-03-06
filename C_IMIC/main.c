de5eae530d8a5141e865470443924a90166e76d6
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-06 19:41:39 +0700

    Find max

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,7 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void main()
 {
-	printf("Xin chao\n");
-	getchar();
+	float a, b;
+	printf("Nhap gia tri cua a: ");
+	scanf("%f", &a);
+	printf("Nhap gia tri cua b: ");
+	scanf("%f", &b);
+	if (a-b > 0)
+	{
+		printf("a la so lon nhat");
+	}
+	else
+	{
+		printf("b la so lon nhat");
+	}
 }
