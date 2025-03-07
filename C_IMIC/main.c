ea72f4e413d22e5c1ebf9f21683ab44304a06e3b
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-07 22:40:14 +0700

    Xác định dấu số nguyên

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,35 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
+
 
 void main()
 {
-	float a, b, c, delta, x1, x2;
+	char check;
+	int a;
+	printf("Nhap so nguyen khac 0\n");
 	while (1)
 	{
-		printf("Nhap cac he so a, b ,c (nhap ky tu de thoat): ");
-		if (scanf("%f %f %f", &a, &b, &c) != 3)
+		if (scanf("%d%c", &a, &check) != 2 || check !='\n')
 		{
-			printf("Thoat chuong trinh");
-			break;
+			printf("Vui long nhap so nguyen\n");
+			while (getchar() != '\n');
+			continue;
 		}
-
-		delta = b * b  - 4 * a * c;
-		printf("delta = %.2f\n", delta);
-		if (delta > 0)
+		if (a > 0)
 		{
-			x1 = (-b - sqrt(delta)) / (2 * a);
-			x2 = (-b + sqrt(delta)) / (2 * a);
-			printf("Phuong trinh co 2 nghiem phan biet x1 = %.2f, x2 = %.2f\n", x1, x2);
+			printf("Ban vua nhap so duong\n");
 		}
-		else if (delta == 0)
+		else if (a == 0)
 		{
-			x1 = -b / (2 * a);
-			printf("Phuong trinh co 1 nghiem x = %.2f\n", x1);
+			printf("Nhap 0 lam gi vay\n");
 		}
 		else
 		{
-			printf("Phuong trinh vo nghiem\n");
+			printf("Ban vua nhap so am\n");
 		}
 	}
 }
