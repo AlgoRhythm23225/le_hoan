353c3647efc3ea7d53bcda0ba075cbaa6cad2450
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-07 20:19:34 +0700

    Giải phương trình bậc 2

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,27 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
 void main()
 {
-	int n,s;
-	printf("Enter password: ");
-	scanf("%d", &n);
-	switch (n)
+	float a, b, c, delta, x1, x2;
+	while (1)
 	{
-	case 1234:
-		printf("Enter the number of money you want to withdraw: ");
-		scanf("%d", &s);
-		if (s > 10000)
+		printf("Nhap cac he so a, b ,c (nhap ky tu de thoat): ");
+		if (scanf("%f %f %f", &a, &b, &c) != 3)
 		{
-			printf("To much");
+			printf("Thoat chuong trinh");
+			break;
+		}
+
+		delta = b * b  - 4 * a * c;
+		printf("delta = %.2f\n", delta);
+		if (delta > 0)
+		{
+			x1 = (-b - sqrt(delta)) / (2 * a);
+			x2 = (-b + sqrt(delta)) / (2 * a);
+			printf("Phuong trinh co 2 nghiem phan biet x1 = %.2f, x2 = %.2f\n", x1, x2);
+		}
+		else if (delta == 0)
+		{
+			x1 = -b / (2 * a);
+			printf("Phuong trinh co 1 nghiem x = %.2f\n", x1);
 		}
 		else
 		{
-			printf("Please take your %d", s);
+			printf("Phuong trinh vo nghiem\n");
 		}
-		break;
-	default:
-		printf("Wrong password");
-		break;
-	}		
+	}
 }
