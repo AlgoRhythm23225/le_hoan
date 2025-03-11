235b5316c773922d9a4d0d06fc6af47928adf651
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-11 19:58:40 +0700

    Bảng cứu chương

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,8 +3,12 @@
 
 int main()
 {
-	for (int i = 1; i <= 10; i++)
-	{
-		printf("2x%d = %d\n", i, 2 * i);
-	}
+    for (int i = 2; i <= 9; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            printf("%d x %d = %d\n", i, j, j * i);
+        } 
+    } 
+    return 0;
 }
