7b99147ef9193a37cff7f73fea1140dd7147c18a
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-12 18:16:31 +0700

    Tổng n số nguyên đầu tiên

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,15 +3,23 @@
 
 int main()
 {
-	int i = 0, n = 0;
-	while (i != 1234)
+	int n = 0, s = 0;
+	printf("Bao nhieu so nguyen dau tien? ");
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
 	{
-		printf("Enter password: ");
-		scanf("%d", &i);
-		n++;
-		if (n == 3)
-			break;
-    }
-	if (n == 3) printf("Please try again after 50 years");
-	else printf("Correct");
+		s += i;
+	}
+	printf("Tong %d so nguyen dau tien la: %d\n", n, s);
+	
+	int a = 0, b = 0, h = 0;
+	printf("Nhap min: ");
+	scanf("%d", &a);
+	printf("Nhap max: ");
+	scanf("%d", &b);
+	for (int i = a; i <= b; i++)
+	{
+		h += i;
+	}
+	printf("Tong cac so nguyen tu %d toi %d la %d", a, b, h);
 }
