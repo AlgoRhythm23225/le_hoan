113b746d0a0264e340f05b7255e69bca52b5473f
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-12 18:19:29 +0700

    Tổng n số chẵn đầu tiên

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,23 +3,13 @@
 
 int main()
 {
-	int n = 0, s = 0;
-	printf("Bao nhieu so nguyen dau tien? ");
+	int n, s = 0;
+	printf("Nhap n so nguyen dau tien: ");
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++)
 	{
-		s += i;
+		if (i % 2 == 0)
+			s += i;
 	}
-	printf("Tong %d so nguyen dau tien la: %d\n", n, s);
-	
-	int a = 0, b = 0, h = 0;
-	printf("Nhap min: ");
-	scanf("%d", &a);
-	printf("Nhap max: ");
-	scanf("%d", &b);
-	for (int i = a; i <= b; i++)
-	{
-		h += i;
-	}
-	printf("Tong cac so nguyen tu %d toi %d la %d", a, b, h);
+	printf("Tong cua %d so chan dau tien la: %d", n, s);
 }
