23898689cf951d18099b98c22e5afb3fda1ef285
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-13 20:30:58 +0700

    Vị trí số lớn nhất

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -4,19 +4,24 @@
 int main()
 {
 	int arr[10] = { 0 };
-	int tong = 0, s = 0;
+	int s = 0, k = 0;
 	for (int i = 0; i < 10; i++)
 	{
 		printf("Nhap so %d: ", i);
 		scanf("%d", &arr[i]);
 	}
 
-	for (int i = 0; i < 8; i++)
+	int so_lon_nhat = 0;
+	int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < 10; i++)
 	{
-		if (arr[i] > arr[i + 1])
-			arr[i + 1] = arr[i];
-		else arr[i + 1] = arr[i + 1];
-		s = arr[i + 1];
+		if (so_lon_nhat < arr[i])
+			so_lon_nhat = arr[i];	 
 	}
-	printf("So lon nhat la : %d", s);
+
+	for (int i= 0; i < 10; i++)
+	{
+		if(arr[i]==so_lon_nhat)
+			printf("%d ",i);
+	}
 }
