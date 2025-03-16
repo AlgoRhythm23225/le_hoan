53736f433bd83041dd1c2cee4ecaa038524ef065
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-17 00:12:51 +0700

    Xây dựng hàm in ra số lẻ

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,22 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void so_le(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] % 2 != 0)
+			printf("%d ",arr[i]);
+	}
+}
+
 int main()
 {
-	int a[8] = { -3,6,3,-8,5,3,-7,5 };
-	for (int i = 0; i < 8; i++)
-	{
-		if (a[i] < 0)
-			a[i] = 0;
-		printf("%d ", a[i]);	
-	}
-
-	printf("\nSo am da doi thanh 0, tai cac vi tri: ");
-
-	for (int i = 0; i < 8; i++)
-	{
-		if (a[i] == 0)
-			printf("%d ", i);
-	}
-
+	int arr[] = { 1,2,4,3,7,6,8,5,6,7,5,4 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	so_le(arr, size);
 }
