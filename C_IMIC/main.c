31c6c2c1accd6e3cf92b1583a0c964f75415aa58
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-17 00:27:09 +0700

    Xây dựng hàm in giá trị max

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,25 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void inNguyenTo(int arr[], int size)
+void inMax(int arr[], int size)
 {
+	int temp = 0;
 	for (int i = 0; i < size; i++)
 	{
-		int k = 0;
-		for (int j = 1; j <= arr[i]; j++)
-		{
-			if (arr[i] % j == 0)
-				k++;
-		}
-		if (k == 2)
-			printf("%d ", arr[i]);
+		if (arr[i] > temp)
+			temp = arr[i];
 	}
+	printf("So lon nhat trong mang la:\n%d", temp);
 }
 
 int main()
 {
-	int arr[] = { 1,7,6,4,5,8,7,3 };
+	int arr[] = { 1,3,5,3,6,5,7,3,5,54,353,234 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-	printf("Cac so nguyen to trong mang la:\n");
-	inNguyenTo(arr, size);
+	inMax(arr, size);
 }
