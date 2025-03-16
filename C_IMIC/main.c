bb80a39e6a4670d77077c3b9d9c1d36aa9c83581
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-17 00:21:06 +0700

    Xây dựng hàm in ra số nguyên tố

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,18 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void so_le(int arr[], int size)
+void inNguyenTo(int arr[], int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		if (arr[i] % 2 != 0)
-			printf("%d ",arr[i]);
+		int k = 0;
+		for (int j = 1; j <= arr[i]; j++)
+		{
+			if (arr[i] % j == 0)
+				k++;
+		}
+		if (k == 2)
+			printf("%d ", arr[i]);
 	}
 }
 
 int main()
 {
-	int arr[] = { 1,2,4,3,7,6,8,5,6,7,5,4 };
+	int arr[] = { 1,7,6,4,5,8,7,3 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-	so_le(arr, size);
+	printf("Cac so nguyen to trong mang la:\n");
+	inNguyenTo(arr, size);
 }
