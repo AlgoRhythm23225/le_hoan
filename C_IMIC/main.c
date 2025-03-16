799541126ed534a385121f2d6096a61143a62b6f
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-16 16:07:00 +0700

    Tổng số lẻ trong mảng

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,108 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-	int n;
-	char a[12];
-	printf("Nhap so nguyen tu 6 chu so tro xuong ( khong nhap so co so 0 do chuong trinh chua hoan thien :< ): ");
-	scanf("%d", &n);
-
-	//dem ky tu
-	sprintf(a, "%d", n);
-	int count = strlen(a);
-	for (int i = 0; i < count; i++)
+	//Cap phat tinh
+	int a[12], s = 0;
+	for (int i = 0; i < 12; i++)
 	{
-		switch (a[i])
-		{
-			case '1':
-				printf("mot");
-				break;
-			case '2':
-				printf("hai");
-				break;
-			case '3':
-				printf("ba");
-				break;
-			case '4':
-				printf("bon");
-				break;
-			case '5':
-				printf("nam");
-				break;
-			case '6':
-				printf("sau");
-				break;
-			case '7':
-				printf("bay");
-				break;
-			case '8':
-				printf("tam");
-				break;
-			case '9':
-				printf("chin");
-				break;
-		}
-		if (i != count - 1) printf(" ");
-	
-
-		switch (count - i)
-		{
-			case 7:
-				printf("trieu ");
-				break;
-			case 6:
-				printf("tram ");
-				break;
-			case 5:
-				printf("muoi ");
-				break;
-			case 4:
-				printf("nghin ");
-				break;
-			case 3:
-				printf("tram ");
-				break;
-			case 2:
-				if (a[i] == '0') 
-					printf("linh ");
-				else 
-					printf("muoi ");
-				break;
-			case 1:
-				switch (a[i])
-				{
-					case 1:	
-						printf("mot");
-						break;
-					case 2:	
-						printf("hai");
-						break;
-					case 3:	
-						printf("ba");
-						break;
-					case 4:	
-						printf("bon");
-						break;
-					case 5:	
-						printf("nam");
-						break;
-					case 6:	
-						printf("sau");
-						break;
-					case 7:	
-						printf("bay");
-						break;
-					case 8:	
-						printf("tam");
-						break;
-					case 9:	
-						printf("chin");
-						break;
-				}
-				break;
-		}
+		printf("Nhap so thu %d: ", i);
+		scanf("%d", &a[i]);
+		if (a[i] % 2 != 0)
+			s += a[i];
 	}
-	
+	printf("%d", s);
 }
