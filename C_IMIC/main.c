f70b71564b12f281d9cb76b57555de806ff46342
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-17 12:10:22 +0700

    Xây dựng hàm thay đổi vị trí

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,20 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void inMax(int arr[], int size)
+void pos_swap(int arr[], int a, int b)
 {
-	int temp = 0;
-	for (int i = 0; i < size; i++)
+	int temp1 = 0, pos1 = 0, temp2 = 0, pos2 = 0;
+	for (int i = 0; i < 14; i++)
 	{
-		if (arr[i] > temp)
-			temp = arr[i];
+		if (arr[i] == a)
+		{
+			temp1 = a;
+			pos1 = i;
+		}
+		if (arr[i] == b)
+		{
+			temp2 = b;
+			pos2 = i;
+		}
 	}
-	printf("So lon nhat trong mang la:\n%d", temp);
+	arr[pos1] = temp2;
+	arr[pos2] = temp1;
 }
 
 int main()
 {
-	int arr[] = { 1,3,5,3,6,5,7,3,5,54,353,234 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-	inMax(arr, size);
+	int arr[] = { 1,4,2,6,8,7,0,12,14,17,19,25,74,58 };
+	int a, b;
+	for (int i = 0;i<14;i++)
+	{ 
+		printf("%d ", arr[i]);
+	}
+	printf("\nNhap 2 so trong mang ban muon thay doi vi tri: ");
+	scanf("%d%d", &a, &b);
+	pos_swap(arr, a, b);
+	for (int i = 0; i < 14; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 }
