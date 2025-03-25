7720a10d9c627287d75c8ae05613f2fd35fae5f5
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-25 19:46:17 +0700

    tích

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,61 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-// bài 1: ?i?m s? ký t? trong chu?i
-// input:
-//	char* str: chu?i c?n tìm 
-// output: int s? l??ng ký t? trong chu?i
-int Tim_So_Ky_Tu(char* str)
+struct phan_so
 {
-	int diem = 0;
-	while (str[diem] != 0)
-	{
-		diem++;
-	}
-	return diem;
+	int tu;
+	int mau;
+};
+
+//Chuc nang: Nhan 2 phan so
+//Input:
+//Output:
+float nhan_2_phan_so(float phan_so_1, float phan_so_2)
+{
+	float ket_qua = phan_so_1 * phan_so_2;
+	return ket_qua;
 }
 
-
-// bài 6: tìm ký t? c trong chu?i str
-// input:
-//	char* str : chu?i c?n tìm
-//	char c	  : ký t? c?n tìm
-// output: char* ??a ch? c?a c trong str
-char* Tim_Ky_Tu(char* str, char c)
+int main()
 {
-	int chieu_dai_chuoi = Tim_So_Ky_Tu(str);
-	for (int i = 0; i < chieu_dai_chuoi; i++)
-	{
-		if (str[i] == c)
-			return str + i;
-	}
-	return 0;
-}
+	struct phan_so A;
+	struct phan_so B;
+	
+	A.tu = 1;
+	A.mau = 2;
 
-// bài 7: tìm chu?i subStr trong chu?i str
-// input:
-//	char* str: chu?i l?n c?n tìm
-//	char* subStr: chu?i nh? c?n tìm
-// output: char* ??a ch? c?a subStr trong str
-char* Tim_Chuoi(char* str, char* subStr)
-{
-	int chieu_dai_str = Tim_So_Ky_Tu(str);
-	int chieu_dai_subStr = Tim_So_Ky_Tu(subStr);
-	for (int i = 0; i < chieu_dai_str; i++)
-	{
-		int j = 0;
-		for (; j < chieu_dai_subStr; j++)
-		{
-			if (str[i + j] != subStr[j])
-				break;
-		}
-		if (j == chieu_dai_subStr)
-			return str + i;
-	}
-	return 0;
-}
+	B.tu = 3;
+	B.mau = 4;
 
-void main()
-{
-	int x = cong(3, 4);
-	printf("%d", x);
+	float phan_so_1 = A.tu / A.mau;
+	float phan_so_2 = B.tu / B.mau;
+	float tich = nhan_2_phan_so(phan_so_1, phan_so_2);
+
+	printf("%.2f", tich);
 }
--- C_IMIC/toan.h
+++ C_IMIC/toan.h
@@ -1,2 +1,4 @@
 int cong(int x, int y);
 int nhan(int x, int y);
+float chia(int x, int y);
+int giai_thua(int x);
