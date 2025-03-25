3949962ba0171365c849bf4bdd27c92b008c0ade
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-03-25 20:27:55 +0700

    Học sinh

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,26 +3,21 @@
 
 typedef struct
 {
-	int tu;
-	int mau;
-}phan_so;
-
-//Chuc nang: Nhan 2 phan so
-//Input:
-//Output:
-phan_so NhanPhanSo(phan_so x, phan_so y)
-{
-	phan_so kq;
-	kq.tu = x.tu * y.tu;
-	kq.mau = x.mau * y.mau;
-	return kq;
-}
+	char ten[40];
+	int tuoi;
+	float diem_toan;
+	float diem_van;
+	float diem_tb;
+	int xep_loai;
+}Hoc_sinh;
 
 int main()
 {
-	phan_so A = { 1,2 };
-	phan_so B = { 3,4 };
-	
-	phan_so C = NhanPhanSo(A, B);
-	printf("Ket qua:%d/%d \n", C.tu, C.mau);
+	Hoc_sinh hocsinh[] = {
+		{"Quang", 15, 8, 9, 7.5, 0},
+		{"Duc", 14, 6, 9, 7.5, 0},
+		{"Linh", 15, 6, 7.5, 7.5, 0},
+		{"Mai", 14, 8.6, 4, 7.5, 0}
+	};
+
 }
