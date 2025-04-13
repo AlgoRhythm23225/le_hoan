32794eec8db87310222760d5aa2ced3200333bf4
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-04-13 23:38:32 +0700

    Thread - Pipe - PeekNamedPipe

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,29 +1,51 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
+#include <Windows.h>
 
-typedef union
+HANDLE ReadPipe;
+HANDLE WritePipe;
+
+DWORD WINAPI function_1(_In_ LPVOID lp)
 {
-	unsigned short x;
-	struct {
-		unsigned char y_low;
-		unsigned char y_high;
-	};
-}union_t;
+	int cnt = 0;
+	while (1)
+	{
+		cnt++;
+		WriteFile(WritePipe, &cnt, sizeof(cnt), NULL, NULL);
+		printf("function_1 is running...\n");
+		Sleep(100);
+	}
+}
 
-int main() 
+void main()
 {
-	union_t A;
-	unsigned short val = 0x1234;
-	unsigned char low;
-	unsigned char high;
-
-	A.x = val;
-	low = A.y_low;
-	high = A.y_high;
+	//Tao Thread
+	HANDLE thread_1 = CreateThread(NULL, 1024, function_1, NULL, 0, NULL);
 	
-	printf("Value = 0x%X\n", val);
-	printf("Low = 0x%X\n", low);
-	printf("High = 0x%X\n", high);
+	//Tao Pipe
+	CreatePipe(&ReadPipe, &WritePipe, NULL, 1024);
+	int temp = 0;
+	printf("Data:\n");
+	int buffer[40];
+	while (1)
+	{
+		int num_data_read = 0;
+		int num_data_read_0 = 0;
+		PeekNamedPipe(ReadPipe, buffer, sizeof(buffer), &num_data_read_0, &num_data_read, NULL);
+		
+		//data read available
+		num_data_read /= 4;
 
+		num_data_read_0 /= 4;
+		for (int i = 0; i < num_data_read; i++)
+		{
+			ReadFile(ReadPipe, &temp, sizeof(temp), NULL, NULL);
+			printf("%d ", temp);
+		}
+		printf("\n");
+
+		//Data read
+		printf("Num_data_read_0: %d",num_data_read_0);
+		printf("\n");
+		Sleep(4000);
+	}
 }
