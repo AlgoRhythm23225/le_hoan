1e918a0be6e5d2885c9a4e2a42a288dded7ade83
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-04-14 11:54:02 +0700

    WaitForSingleObject, CreateMutex

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,51 +1,40 @@
 #include <stdio.h>
 #include <Windows.h>
 
-HANDLE ReadPipe;
-HANDLE WritePipe;
+HANDLE muxtex_lock;
+
+void print_log(char* log)
+{
+	int log_len = strlen(log);
+	for (int i = 0; i < log_len; i++)
+	{
+		printf("%c", log[i]);
+		Sleep(50);
+	}
+}
 
 DWORD WINAPI function_1(_In_ LPVOID lp)
 {
-	int cnt = 0;
 	while (1)
 	{
-		cnt++;
-		WriteFile(WritePipe, &cnt, sizeof(cnt), NULL, NULL);
-		printf("function_1 is running...\n");
+		WaitForSingleObject(muxtex_lock, INFINITE);
+		print_log("function_1 is running...\n");
+		ReleaseMutex(muxtex_lock);
 		Sleep(100);
 	}
 }
 
 void main()
 {
-	//Tao Thread
+	//Create a thread
+	muxtex_lock = CreateMutex(NULL, 0, NULL);
 	HANDLE thread_1 = CreateThread(NULL, 1024, function_1, NULL, 0, NULL);
-	
-	//Tao Pipe
-	CreatePipe(&ReadPipe, &WritePipe, NULL, 1024);
-	int temp = 0;
-	printf("Data:\n");
-	int buffer[40];
+
 	while (1)
 	{
-		int num_data_read = 0;
-		int num_data_read_0 = 0;
-		PeekNamedPipe(ReadPipe, buffer, sizeof(buffer), &num_data_read_0, &num_data_read, NULL);
-		
-		//data read available
-		num_data_read /= 4;
-
-		num_data_read_0 /= 4;
-		for (int i = 0; i < num_data_read; i++)
-		{
-			ReadFile(ReadPipe, &temp, sizeof(temp), NULL, NULL);
-			printf("%d ", temp);
-		}
-		printf("\n");
-
-		//Data read
-		printf("Num_data_read_0: %d",num_data_read_0);
-		printf("\n");
-		Sleep(4000);
+		WaitForSingleObject(muxtex_lock, INFINITE);
+		print_log("main is running...\n");
+		ReleaseMutex(muxtex_lock);
+		Sleep(200);
 	}
 }
