3e006dcf20e84b24f0eab04b219db6de34bfe10c
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-04-15 19:56:14 +0700

    Dùng con trỏ hàm tính tích phân

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,40 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <Windows.h>
+#include <string.h>
+#include <math.h>
 
-HANDLE muxtex_lock;
-
-void print_log(char* log)
+float fx(float x)
 {
-	int log_len = strlen(log);
-	for (int i = 0; i < log_len; i++)
-	{
-		printf("%c", log[i]);
-		Sleep(50);
-	}
+	return x * x;
 }
 
-DWORD WINAPI function_1(_In_ LPVOID lp)
+float gx(float x)
 {
-	while (1)
+	return 2 * x * x + 3 * x + 1;
+}
+
+float tx(float x)
+{
+	return sin(x) + 1;
+}
+
+float tinhTichPhan(int a, int b, float (*f)(float))
+{
+	float h = (b - a) / 10000.0f;
+	float S = 0;
+	for (int i = 0; i < 1000; i++)
 	{
-		WaitForSingleObject(muxtex_lock, INFINITE);
-		print_log("function_1 is running...\n");
-		ReleaseMutex(muxtex_lock);
-		Sleep(100);
+		float db = f(a + i * h);
+		float dl = f(a + (i + 1) * h);
+		S += (dl + db) * h / 2;
 	}
+	return S;
+}
+
+float function_1(int x, char y)
+{
+	printf("Day la FUNCTION 1 x: %d, y: %d\n", x, y);
+	return 3.14f;
+}
+
+typedef void (*pfunc)();
+
+float function_2(int x, char y)
+{
+	printf("Day la FUNCTION 2 x: %d, y: %d\n", x, y);
+	return 3.14f;
+}
+
+void function_3()
+{
+	printf("This is FUNCTION 3\n");
 }
 
 void main()
 {
-	//Create a thread
-	muxtex_lock = CreateMutex(NULL, 0, NULL);
-	HANDLE thread_1 = CreateThread(NULL, 1024, function_1, NULL, 0, NULL);
+	//Khai bao 1 con tro ham, output float, input int vaf char
+	float (*ten_con_tro_ham)(int, char);
+	//Chuc nang 1: dung de chua dia chi ham
+	ten_con_tro_ham = function_2;
 
-	while (1)
-	{
-		WaitForSingleObject(muxtex_lock, INFINITE);
-		print_log("main is running...\n");
-		ReleaseMutex(muxtex_lock);
-		Sleep(200);
-	}
+	//Chuc nang 2: dung de thuc thi ham co dia chi duoc luu trong con tro
+	ten_con_tro_ham(1, 2);
+
+	pfunc pf;
+	pf = function_3;
+	pf();
+
+	float S = tinhTichPhan(4, 12, &fx);
+
+	printf("Tich phan ham fx: %.1f", S);
 }
