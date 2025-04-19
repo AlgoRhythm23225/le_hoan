aa1736205d0bde7132d29202e3c09ccd5a4197aa
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-04-20 00:44:13 +0700

    Tìm điểm trung bình lớn nhất trong file

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,70 +1,95 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-float fx(float x)
+typedef struct
 {
-	return x * x;
-}
-
-float gx(float x)
-{
-	return 2 * x * x + 3 * x + 1;
-}
-
-float tx(float x)
-{
-	return sin(x) + 1;
-}
-
-float tinhTichPhan(int a, int b, float (*f)(float))
-{
-	float h = (b - a) / 10000.0f;
-	float S = 0;
-	for (int i = 0; i < 1000; i++)
-	{
-		float db = f(a + i * h);
-		float dl = f(a + (i + 1) * h);
-		S += (dl + db) * h / 2;
-	}
-	return S;
-}
-
-float function_1(int x, char y)
-{
-	printf("Day la FUNCTION 1 x: %d, y: %d\n", x, y);
-	return 3.14f;
-}
-
-typedef void (*pfunc)();
-
-float function_2(int x, char y)
-{
-	printf("Day la FUNCTION 2 x: %d, y: %d\n", x, y);
-	return 3.14f;
-}
-
-void function_3()
-{
-	printf("This is FUNCTION 3\n");
-}
+	char* ten;
+	int tuoi;
+	int gioi_tinh;
+	float diem_toan;
+	float diem_van;
+}hoc_sinh_t;
 
 void main()
 {
-	//Khai bao 1 con tro ham, output float, input int vaf char
-	float (*ten_con_tro_ham)(int, char);
-	//Chuc nang 1: dung de chua dia chi ham
-	ten_con_tro_ham = function_2;
+	hoc_sinh_t hs = { 0 };
 
-	//Chuc nang 2: dung de thuc thi ham co dia chi duoc luu trong con tro
-	ten_con_tro_ham(1, 2);
+	//Mo file
+	FILE* csv = fopen("DanhSachHocSinh.csv", "r");
+	if (!csv)
+	{
+		printf("Mo file khong thanh cong");
+		return;
+	}
+	else
+	{
+		printf("Mo file thanh cong\n");
+	}
 
-	pfunc pf;
-	pf = function_3;
-	pf();
+	//read
+	char c; 
+	int comma_cnt = 0; //Dem so dau ',' cua 1 line
+	
+	char buffer_toan[3] = { 0 }; //Luu diem toan cua 1 line
+	int buffer_index_toan = 0; //Index trong buffer toan
+	int diem_toan[6] = { 0 };//Luu diem toan cua 1 file
+	int toan_index = 0;
 
-	float S = tinhTichPhan(4, 12, &fx);
+	char buffer_van[3] = { 0 }; //Luu diem vam cua 1 line
+	int buffer_index_van = 0; //Index trong buffer van
+	int diem_van[6] = { 0 };//Luu diem van cua 1 file
+	int van_index = 0;
 
-	printf("Tich phan ham fx: %.1f", S);
+	while ((c = fgetc(csv)) != EOF)
+	{
+		while ((c = fgetc(csv)) != '\n')
+		{
+			if (c == ',')
+			{
+				comma_cnt++;
+			}
+
+			if (comma_cnt == 3 && c != ',')
+			{
+				buffer_toan[buffer_index_toan] = c;
+				buffer_index_toan++;
+			}	
+			
+			if (comma_cnt == 4 && c != ',')
+			{
+				buffer_van[buffer_index_van] = c;
+				buffer_index_van++;
+			}
+		}
+		diem_toan[toan_index] = atoi(buffer_toan);
+		toan_index++;
+		buffer_index_toan = 0;
+		
+		diem_van[van_index] = atoi(buffer_van);
+		van_index++;
+		buffer_index_van = 0;
+
+		comma_cnt = 0;
+	}
+
+	float diem_tb[6] = { 0 };
+	float temp = 0;
+	float index;
+	for (int i = 0; i < 5; i++)
+	{
+		diem_tb[i] = (diem_toan[i] + diem_van[i]) / 2.0f;
+		if (diem_tb[i] > temp)
+		{
+			temp = diem_tb[i];
+			index = i;
+		}
+	}
+	printf("temp: %.1f - index: %d", temp, index);
+
+	
+
+	//Dong file
+	fclose(csv);
 }
