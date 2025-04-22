9bcba2f7bfb7464e2aea29a5975d4797b7518177
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-04-22 16:10:53 +0700

    Đọc dung lương và sample rate file audio.wav ( Update 3 )

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,14 +1,49 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 //Chuc nang: skip so byte can thiet
 //Input: bien FILE
 //Output: skip n file
-void skip(FILE* bin, int number_of_byte_to_skip)
+void skip(FILE* bin, int numbers_of_byte_to_skip)
 {
-	for (int i = 1; i <= number_of_byte_to_skip; i++)
+	for (int i = 1; i <= numbers_of_byte_to_skip; i++)
 	{
 		fgetc(bin);
 	}
+}
+
+void file_size(FILE* bin)
+{
+	rewind(bin);
+	skip(bin,4);
+	unsigned char a5 = fgetc(bin);
+	unsigned char a6 = fgetc(bin);
+	unsigned char a7 = fgetc(bin);
+	unsigned char a8 = fgetc(bin);
+	unsigned int size = a5 | (a6 << 8) | (a7 << 16) | (a8 << 24);
+	printf("Size of audio.wav = %u bytes\n", size);
+}
+
+void sample_rate(FILE* bin)
+{
+	rewind(bin);
+	skip(bin, 24);
+	unsigned char a25 = fgetc(bin);
+	unsigned char a26 = fgetc(bin);
+	unsigned char a27 = fgetc(bin);
+	unsigned char a28 = fgetc(bin);
+	unsigned int hz = a25 | (a26 << 8) | (a27 << 16) | (a28 << 24);
+	printf("Sample rate of audio.wav = %u bytes\n", hz);
+}
+
+void num_channels(FILE* bin)
+{
+	rewind(bin);
+	skip(bin, 22);
+	unsigned char a23 = fgetc(bin);
+	unsigned char a24 = fgetc(bin);
+	unsigned int num_channel = a23 | (a24 << 8);
+	printf("Number of Channels = %u\n", num_channel);
 }
 void main()
 {
@@ -20,23 +55,9 @@ void main()
 	}
 	else printf("Mo file thanh cong\n");
 
-	skip(bin, 4);
-	//Doc 4 byte tiep - Dung luong
-	unsigned char a5 = fgetc(bin);
-	unsigned char a6 = fgetc(bin);
-	unsigned char a7 = fgetc(bin);
-	unsigned char a8 = fgetc(bin);
-	unsigned int size = a5 | (a6 << 8) | (a7 << 16) | (a8 << 24);
-	printf("Size of audio.wav = %u bytes\n", size);
+	file_size(bin);
+	num_channels(bin);
+	sample_rate(bin);
 
-	skip(bin, 16);
-
-	//Doc Sample Rate
-	unsigned char a25 = fgetc(bin);
-	unsigned char a26 = fgetc(bin);
-	unsigned char a27 = fgetc(bin);
-	unsigned char a28 = fgetc(bin);
-
-	unsigned int sample_rate = a25 | (a26 << 8) | (a27 << 16) | (a28 << 24);
-	printf("Sample rate of audio.wav = %u bytes\n", sample_rate);	
+	fclose(bin);
 }
