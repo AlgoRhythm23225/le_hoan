88e616e8ec1a91c355952c7df664332ecf66935b
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-05-13 00:49:45 +0700

    C++ | Class

---
+++ C_IMIC/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include "hinh_hoc.h"
+
+void main()
+{
+	hinh_chu_nhat_c B;
+	B.dai = 5;
+	B.rong = 3;
+	printf("%d\n", B.tinh_dien_tich());
+	printf("%d\n", B.tinh_chu_vi());	
+}
