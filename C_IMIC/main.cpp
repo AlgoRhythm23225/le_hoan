aba4913dc325e93e3db84b749d95c3fd35483114
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-05-20 23:35:35 +0700

    C++: Class - Tạo mảng, thêm phần tử vào mảng

--- C_IMIC/main.cpp
+++ C_IMIC/main.cpp
@@ -1,35 +1,82 @@
-﻿#include "phan_so.h"
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <iostream>
 
-int main()
+class mang
 {
-	phan_so a(2, 4), b(6, 4);
-	phan_so mult, div, add, subt, simpl;
-	
-	printf("a = %d / %d\n", a.tu, a.mau);
-	printf("b = %d / %d\n\n", b.tu, b.mau);
-	
-	mult = a * b;
-	mult = mult.simplify();
-	printf("a x b = %d / %d\n", mult.tu, mult.mau);
-	
-	div = a / b;
-	div = div.simplify();
-	printf("a %% b = %d / %d\n", div.tu, div.mau);
-	
-	add = a + b;
-	add = add.simplify();
-	if (add.mau == 1)
-		printf("a + b = %d\n", add.tu);
-	else
-		printf("a + b = %d / %d\n", add.tu, add.mau);
+public:
+	int* data;
+	int size;
 
-	subt = a - b;
-	subt = subt.simplify();
-	if (subt.tu == 0)
-		printf("a - b = %d\n", 0);
-	else
-		printf("a - b = %d / %d\n", subt.tu, subt.mau);
+	mang(int num_item)
+	{
+		data = (int*)malloc(num_item * sizeof(int));
+		if (data == NULL)
+			std::runtime_error("Khong the dung malloc de cap phat vung nho");
+		size = num_item;
+	}
 
-	simpl = a.simplify();
-	a.compare(b);
+	int& operator[](int index)
+	{
+		if (index >= size)
+			throw std::runtime_error("Vuot qua so luong phan tu trong mang");
+		return data[index];
+	}
+
+	void push_back(int val)
+	{
+		// Cấp phát vùng nhớ mới lớn hơn vùng nhớ cũ
+		int* new_temp = (int*)malloc((size + 1) * sizeof(int));
+		if (new_temp == NULL)
+			throw std::runtime_error("Khong the cap phat vung nho moi");
+		// Copy dữ liệu từ vùng nhớ cũ sang vùng nhớ mới và set giá trị của đối tượng cuối cùng
+		for (int i = 0; i < size; i++)
+		{
+			new_temp[i] = data[i];
+		}
+		new_temp[size] = val;
+		// Hủy vùng nhớ cũ
+		free(data);
+		// Cập nhật địa chỉ của vùng nhớ mới vào con trỏ data
+		data = new_temp;
+		// Cập nhật lại giá trị của biến size
+		size++;
+	}
+
+	~mang()
+	{
+		free(data);
+	}
+};
+
+int sum_arr(mang& a)
+{
+	int sum = 0;
+	for (int i = 0; i < a.size; i++)
+	{
+		sum += a[i];
+	}
+	return sum;
+}
+
+
+
+void main()
+{
+	mang a(3);
+
+	try
+	{
+		a[0] = 2;
+		a[1] = 5;
+		a[2] = 3;
+		a.push_back(4);
+		printf("Sum array: %d", sum_arr(a));
+	}
+	catch (std::exception e)
+	{
+		printf("Something went wrong (%s)\n",e.what());
+	}
+
+	
 }
