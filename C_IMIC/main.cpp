1f0d0e2f29fe6d96fd5e0e85c6cce788ebbaa54e
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-05-29 18:50:16 +0700

    Câu 2: Xây dựng Class giải phương trình bậc 2

--- C_IMIC/main.cpp
+++ C_IMIC/main.cpp
@@ -1,14 +1,51 @@
-﻿#include <stdio.h>
+﻿#include <iostream>
+#include <cmath>
 
-void print_result(int value) {
-    int high = (value >> 8) & 0xFF; 
-    int low = value & 0xFF;        
+class phuong_trinh_bac_hai {
+private:
+    float a, b, c;
 
-    printf("High byte: %d (0x%02X)\n", high, high);
-    printf("Low byte: %d (0x%02X)\n", low, low);
-}
+public:
+    phuong_trinh_bac_hai(float a, float b, float c) : a(a), b(b), c(c) {}
 
-void main() {
-    int num = 4325;
-    print_result(num);
+    void solve() {
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0)
+                    std::cout << "Phuong trinh vo so nghiem.\n";
+                else
+                    std::cout << "Phuong trinh vo nghiem.\n";
+            }
+            else {
+                float x = -c / b;
+                std::cout << "Phuong trinh bac nhat co nghiem: x = " << x << "\n";
+            }
+        }
+        else {
+            float delta = b * b - 4 * a * c;
+            if (delta > 0) {
+                float x1 = (-b + sqrtf(delta)) / (2 * a);
+                float x2 = (-b - sqrtf(delta)) / (2 * a);
+                std::cout << "Phuong trinh co 2 nghiem phan biet:\n";
+                std::cout << "x1 = " << x1 << ", x2 = " << x2 << "\n";
+            }
+            else if (delta == 0) {
+                float x = -b / (2 * a);
+                std::cout << "Phuong trinh co nghiem kep: x = " << x << "\n";
+            }
+            else {
+                std::cout << "Phuong trinh vo nghiem (delta < 0).\n";
+            }
+        }
+    }
+};
+
+void main() 
+{
+    float a, b, c;
+    std::cout << "Nhap a, b, c: ";
+    std::cin >> a >> b >> c;
+
+    phuong_trinh_bac_hai eq(a, b, c);
+    eq.solve();
 }
