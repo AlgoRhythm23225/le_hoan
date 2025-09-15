ad267b6d4760f3d7112579750e86cb7a355fce06
AlgoRhythm23225 <nightmare3198@gmail.com>
2025-09-15 22:04:03 +0700

    Class: Push_back

--- C_IMIC/main.cpp
+++ C_IMIC/main.cpp
@@ -1,32 +1,68 @@
-﻿#include "phan_so.h"
+﻿#include <iostream>
 
-void main()
+using namespace std;
+class Mang 
 {
-	phan_so_c A, B, C, D, E, F, G(2,1);
-	A.tu = 5;
-	A.mau = 14;
+public:
+	int* data;
+	int size;
+	int cap;
 
-	B.tu = 6;
-	B.mau = 7;
+	Mang(int num_item) : data(new int[num_item]), size(num_item), cap(num_item) 
+	{
+		if (data == NULL)
+			throw runtime_error("Khong the cap phat vung nho moi");
+	}
 
-	C = A * B; 
-	C = C.rut_gon(C);
+	int& operator[](int index)
+	{
+		if (index < 0 || index >= size)
+			throw runtime_error("Vuot qua so luong phan tu");
+		return data[index];
+	}
 
-	D = A / B;
-	D = D.rut_gon(D);
+	void grow()
+	{
+		int new_cap = (cap == 0 ? 1 : cap * 2);
+		if (new_cap == NULL)
+			throw runtime_error("Khong the cap phat vung nho moi");
+		int* nd = new int[new_cap];
+		for (int i = 0; i < size; i++)
+		{
+			nd[i] = data[i];
+		}
+		delete[] data;
+		data = nd;
+		cap = new_cap;
+	}
 
-	E = A + B;
-	E = E.rut_gon(E);
+	void push_back(int val)
+	{
+		if (size == cap) grow();
+		data[size++] = val;
+	}
 
-	F = A - B; 
-	F = F.rut_gon(F);
+	~Mang()
+	{
+		delete[] data;
+	}
+};
 
-	/*cout << "Phan so A: " << A.tu<<"/" << A.mau << endl;
-	cout << "Phan so B: " << B.tu<<"/" << B.mau << endl;
-	cout << "Phan so C = A x B: " << C.tu<<"/" << C.mau << endl;
-	cout << "Phan so D = A / B: " << D.tu<<"/" << D.mau << endl;
-	cout << "Phan so E = A + B: " << E.tu<<"/" << E.mau << endl;
-	cout << "Phan so F = A - B: " << F.tu<<"/" << F.mau << endl;*/
-	
-	
+int main()
+{
+	Mang a(3);
+	a[0] = 4;
+	a[1] = 3;
+	a[2] = 8;
+	try
+	{
+		a[3] = 8;
+	} 
+	catch (const exception& e)
+	{
+		cout << "Loi: " << e.what() << endl;
+	}
+	cout << "a[2] = " << a[2] << endl;
+	a.push_back(10);
+	cout << "a[3] = " << a[3] << endl;
 }
